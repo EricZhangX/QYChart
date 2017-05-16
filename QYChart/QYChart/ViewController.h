6b7fa5897099e5b2ca4b978b1b3bcc489f7469b3
zqy <zhangqingyu12@126.com>
2017-05-16 15:03:19 +0800

    no message

---
+++ QYChart/QYChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYChart
+//
+//  Created by 张庆玉 on 16/05/2017.
+//  Copyright © 2017 张庆玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QYChart/QYChart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QYChart
+//
+//  Created by 张庆玉 on 16/05/2017.
+//  Copyright © 2017 张庆玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
